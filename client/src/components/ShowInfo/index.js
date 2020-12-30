e68877360eae244ec4c4b7b27b4ae3fc7dfb4244
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-12-30 00:29:18 -0500

    continue layout of ShowInfo

--- client/src/components/ShowInfo/index.js
+++ client/src/components/ShowInfo/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './style.css';
+import dateFunction from '../../other/dateFunction';
 
 function ShowInfo(props) {
     const {showNum, date, venue, address, city, stateCountry} = props;
+
+    const dateString = dateFunction(date);
+
     return (
-        <div className='grid-container'>
-            <div className='date'>{date}</div>
-            <div className='show-num'># {showNum}</div>
-            <div className='venue-title'>Venue:</div>
-            <div className='venue'>{venue}</div>
-            <div className='address-title'>Address:</div>
-            <div className='street'>{address}</div>
-            <div className='city'>{city}, {stateCountry}</div>
-            {/* <p>{showNum} - {date} - {venue} - {address} - {city} - {stateCountry}</p> */}
+        <div className='show-info-container'>
+            <div className='grid-container'>
+                <div className='date'><h4 class='show-date-heading'>{dateString}</h4></div>
+                <div className='show-num'># {showNum}</div>
+                <div className='venue-title'><p className='slight-bold'>Venue:</p></div>
+                <div className='venue'>{venue}</div>
+                <div className='address-title'><p className='slight-bold'>Address:</p></div>
+                <div className='street'>{address}</div>
+                <div className='city'>{city}, {stateCountry}</div>
+            </div>
+            <div className='show-info-border'></div>
         </div>
     );
 };
---
+++ client/src/other/dateFunction.js
@@ -0,0 +1,38 @@
+const dateFunction = (yyyymmdd) => {
+    const yearNum = yyyymmdd.slice(0,4);
+    const monthNum = yyyymmdd.slice(5,7);
+    const dayNum = yyyymmdd.slice(8,10);
+
+    let monthName;
+
+    if(monthNum==='01') {
+        monthName='Jan';
+    } else if (monthNum==='02') {
+        monthName='Feb'
+    } else if (monthNum==='03') {
+        monthName='Mar'
+    } else if (monthNum==='04') {
+        monthName='Apr'
+    } else if (monthNum==='05') {
+        monthName='May'
+    } else if (monthNum==='06') {
+        monthName='Jun'
+    } else if (monthNum==='07') {
+        monthName='Jul'
+    } else if (monthNum==='08') {
+        monthName='Aug'
+    } else if (monthNum==='09') {
+        monthName='Sep'
+    } else if (monthNum==='10') {
+        monthName='Oct'
+    } else if (monthNum==='11') {
+        monthName='Nov'
+    } else if (monthNum==='12') {
+        monthName='Dec'
+    }
+
+    const dateString = `${monthName} ${dayNum}, ${yearNum}`;
+    return dateString;
+};
+
+export default dateFunction;
